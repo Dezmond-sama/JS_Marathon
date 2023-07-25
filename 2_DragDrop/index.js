b0a5ba97ec9caea0fd3cb84df50ea6284bae31f6
Dezmond-sama <Dezmond-sama@mail.ru>
2023-07-25 17:05:22 +0300

    Drop functions and list of draggable components

--- 2_DragDrop/index.js
+++ 2_DragDrop/index.js
@@ -1,29 +1,42 @@
-const dragable = document.querySelector(".item");
+let dragged = null;
+
+const dragable = document.querySelectorAll(".item");
 const dropPositions = document.querySelectorAll(".placeholder");
 
 const dragstart = ({ target }) => {
+    dragged = target;
     target.classList.add("drag");
     setTimeout(() => target.classList.add("hidden"), 0);
 };
 const dragend = ({ target }) => {
+    dragged = null;
     target.classList.remove("drag", "hidden");
 };
 const dragenter = ({ target }) => {
-    target.classList.add("draghover");
+    let placeholder = target.closest(".placeholder");
+    placeholder.classList.add("draghover");
 };
-const dragleave = ({ target }) => {
-    target.classList.remove("draghover");
+const dragleave = (event) => {
+    //dragleave срабатывает и при наведении на дочерние элементы, из-за этого выделение не работает корректно
+    //решение со сцетчиком помогает, но при быстром перемещении мыши счетчик сбивается, поэтому нужно другое решение
+    let placeholder = event.target.closest(".placeholder");
+    placeholder.classList.remove("draghover");
 };
 const dragover = (event) => {
     event.preventDefault();
 };
 const dragdrop = ({ target }) => {
-    target.classList.remove("draghover");
-    console.log("dragdrop");
+    let placeholder = target.closest(".placeholder");
+    placeholder.classList.remove("draghover");
+    if (dragged) {
+        placeholder.append(dragged);
+    }
 };
 
-dragable.addEventListener("dragstart", dragstart);
-dragable.addEventListener("dragend", dragend);
+dragable.forEach((elem) => {
+    elem.addEventListener("dragstart", dragstart);
+    elem.addEventListener("dragend", dragend);
+});
 
 dropPositions.forEach((elem) => {
     elem.addEventListener("dragleave", dragleave);
